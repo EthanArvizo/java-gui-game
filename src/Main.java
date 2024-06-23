13a28b0ac980ac22e7e92052b8ac06df4b93936a
Ethan <Ethanarvizo8@gmail.com>
2024-06-23 16:24:40 -0700

    java gui

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+/*
+        JFrame frame = new JFrame();
+        frame.setTitle("JFrame Title");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(420,420);
+        frame.setVisible(true);
+
+        ImageIcon image = new ImageIcon("icon.jpg");
+        frame.setIconImage(image.getImage());
+        frame.getContentPane().setBackground(new Color(57, 55, 54 ));
+
+ */
+
+        MyFrame myFrame = new MyFrame();
+    }
+}
