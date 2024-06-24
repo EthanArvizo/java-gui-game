5cd7b6525fabb3bbc60c1e526022fa05e877e63c
Ethan <Ethanarvizo8@gmail.com>
2024-06-23 19:44:24 -0700

    added pack method to adjust to components

--- src/Labels.java
+++ src/Labels.java
@@ -5,8 +5,15 @@ public class Labels {
        JLabel label = new JLabel();
        label.setText(text);
        label.setIcon(icon);
+       label.setHorizontalTextPosition(JLabel.CENTER);
+       label.setVerticalTextPosition(JLabel.TOP);
        label.setForeground(foregroundColor);
        return label;
    }
+    public JLabel createLabel(String text, ImageIcon icon, Color foregroundColor, Font font) {
+        JLabel label = createLabel(text, icon, foregroundColor);
+        label.setFont(font);
+        return label;
+    }
 
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -6,17 +6,27 @@ public class MyFrame extends JFrame {
 
     MyFrame(){
 
-        this.setTitle("JFrame Title");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.setSize(500,500);
-        this.getContentPane().setBackground(new Color(57, 55, 54));
-        this.setVisible(true);
+        setTitle("JFrame Title");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        //setSize(500,500);
+        getContentPane().setBackground(new Color(57, 55, 54));
+        //setLayout(null);
+
 
         labels = new Labels();
 
         ImageIcon image = new ImageIcon("icon.jpg");
-        JLabel label = labels.createLabel("Bro, throw the fireball.",image, Color.white);
+
+        Font customFont = new Font("Serif", Font.BOLD, 20);
+
+        JLabel label = labels.createLabel("Bro, throw the fireball.",image, Color.black, customFont);
+        label.setVerticalAlignment(JLabel.CENTER);// set vertical position of icon+text of label
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setIconTextGap(-20);
+        //label.setBounds(100,0,250,250);
         add(label);
+        pack();
+        setVisible(true);
     }
 }
